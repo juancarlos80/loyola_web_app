9866493e562ee3494135f7dab4bc1e2e385d1760
Juan Carlos Crespo <juan.k.80@gmail.com>
2021-04-07 17:50:39 -0400

    borrar fotos antiguas cuando se actualizan

--- services/upload_user_picture.php
+++ services/upload_user_picture.php
@@ -18,13 +18,7 @@ if( !isset($_POST["type_photo"]) ){
       "reason" => "No se envio el tipo de foto"
   )));  
 }
-$type = $_POST["type_photo"];
-  /*echo json_encode(array(
-      "success" => true,
-      "name" => $_FILES['imageFile']['name'],
-      "email" => $_POST['email']
-  ));
-  die();*/
+$type = $_POST["type_photo"];  
   
 $user = null;  
 if( isset($_POST["oauth_uid"]) ){
@@ -74,22 +68,37 @@ if( !file_exists($tempFile) ){
   )));
 }
 
+//Guardamos la referencia en la bd
+if( $type == "picture_1"){  
+  //Antes de eliminar preguntamos si habia antes una imagen para borrarla
+  if( $user->picture_id_1 != "" && file_exists("../uploads/".$user->id."/".$user->picture_id_1 ) ){
+    unlink("../uploads/".$user->id."/".$user->picture_id_1);
+  }
+  
+  $user->picture_id_1 = $_FILES['imageFile']['name'];
+}
+if( $type == "picture_2"){
+  //Antes de eliminar preguntamos si habia antes una imagen para borrarla
+  if( $user->picture_id_2 != "" && file_exists("../uploads/".$user->id."/".$user->picture_id_2 ) ){
+    unlink("../uploads/".$user->id."/".$user->picture_id_2);
+  }
+  
+  $user->picture_id_2 = $_FILES['imageFile']['name'];
+}
+if( $type == "selfie"){
+  //Antes de eliminar preguntamos si habia antes una imagen para borrarla
+  if( $user->selfie != "" && file_exists("../uploads/".$user->id."/".$user->selfie ) ){
+    unlink("../uploads/".$user->id."/".$user->selfie);
+  }
+  
+  $user->selfie = $_FILES['imageFile']['name'];
+}
+
 if( !move_uploaded_file($tempFile, $targetFile) ){
   die( json_encode(array(
         "success" => false,
         "reason" => "No se pudo guardar la fotografia en el servidor ".$tempFile
   )));
-}
-
-//Guardamos la referencia en la bd
-if( $type == "picture_1"){
-  $user->picture_id_1 = $_FILES['imageFile']['name'];
-}
-if( $type == "picture_2"){
-  $user->picture_id_2 = $_FILES['imageFile']['name'];
-}
-if( $type == "selfie"){
-  $user->selfie = $_FILES['imageFile']['name'];
 }
 
 if( !$user->save() ){
